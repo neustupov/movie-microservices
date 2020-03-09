c58930d1c3f628fea339340a640b47327d738100
Dmitry_Neustupov <neustupov@yandex.ru>
2020-03-09 14:25:40 +0300

    Added timeout for requests

--- movie-catalog-service/src/main/java/org/neustupov/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/org/neustupov/resources/MovieCatalogResource.java
@@ -1,5 +1,6 @@
 package org.neustupov.resources;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.extern.log4j.Log4j2;
@@ -8,6 +9,7 @@ import org.neustupov.models.Movie;
 import org.neustupov.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,11 @@ import org.springframework.web.reactive.function.client.WebClient.Builder;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+  private final String RATINGS_DATA_SERVICE_USERS_URI = "http://ratings-data-service/ratingsdata/users/";
+  private final String MOVIE_INFO_SERVICE_MOVIES_URI = "http://movie-info-service/movies/";
+
+  private final Integer timeout = 5000;
+
   @Qualifier("webClientBuilder")
   private final WebClient.Builder webClientbuilder;
 
@@ -31,22 +38,38 @@ public class MovieCatalogResource {
   @GetMapping("/{userId}")
   public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
-    log.info("Request to: http://ratings-data-service/ratingsdata/users/" + userId);
+    log.info("Request to: " + RATINGS_DATA_SERVICE_USERS_URI + userId);
+
     UserRating userRating = webClientbuilder.build()
         .get()
-        .uri("http://ratings-data-service/ratingsdata/users/" + userId)
+        .uri(RATINGS_DATA_SERVICE_USERS_URI + userId)
         .retrieve()
+        .onStatus(HttpStatus::isError, clientResponse -> {
+          log.error("Error while calling endpoint {} with status code {}",
+              RATINGS_DATA_SERVICE_USERS_URI, clientResponse.statusCode());
+          throw new RuntimeException("Error while calling  accounts endpoint");
+        })
         .bodyToMono(UserRating.class)
+        .timeout(Duration.ofMillis(timeout))
+        .doOnError(error -> log.error("Error signal detected", error))
         .block();
 
     return userRating.getUserRatings().stream().map(rating -> {
-      log.info("Request to: http://movie-info-service/movies/" + rating.getMovieId());
+
+      log.info("Request to: " + MOVIE_INFO_SERVICE_MOVIES_URI + rating.getMovieId());
 
       Movie movie = webClientbuilder.build()
           .get()
-          .uri("http://movie-info-service/movies/" + rating.getMovieId())
+          .uri(MOVIE_INFO_SERVICE_MOVIES_URI + rating.getMovieId())
           .retrieve()
+          .onStatus(HttpStatus::isError, clientResponse -> {
+            log.error("Error while calling endpoint {} with status code {}",
+                MOVIE_INFO_SERVICE_MOVIES_URI, clientResponse.statusCode());
+            throw new RuntimeException("Error while calling  accounts endpoint");
+          })
           .bodyToMono(Movie.class)
+          .timeout(Duration.ofMillis(timeout))
+          .doOnError(error -> log.error("Error signal detected", error))
           .block();
 
       return new CatalogItem(movie.getTitle(), movie.getOverview(), movie.getVote_average());
--- movie-info-service/src/main/java/org/neustupov/resource/MovieResource.java
+++ movie-info-service/src/main/java/org/neustupov/resource/MovieResource.java
@@ -1,11 +1,13 @@
 package org.neustupov.resource;
 
+import java.time.Duration;
 import lombok.extern.log4j.Log4j2;
 import org.neustupov.models.Movie;
 import org.neustupov.models.MovieSummary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +21,8 @@ import org.springframework.web.reactive.function.client.WebClient.Builder;
 public class MovieResource {
 
   private final String DB_URI = "https://api.themoviedb.org/3/movie/";
+
+  private final Integer timeout = 3000;
 
   @Value("${api.key}")
   private String apiKey;
@@ -35,11 +39,19 @@ public class MovieResource {
   public Movie getMovieInfo(@PathVariable("movieId") String movieId){
 
     log.info(DB_URI + movieId);
+
     MovieSummary movieSummary = webClientbuilder.build()
         .get()
         .uri(DB_URI + movieId + "?api_key=" + apiKey)
         .retrieve()
+        .onStatus(HttpStatus::isError, clientResponse -> {
+          log.error("Error while calling endpoint {} with status code {}",
+              DB_URI, clientResponse.statusCode());
+          throw new RuntimeException("Error while calling  accounts endpoint");
+        })
         .bodyToMono(MovieSummary.class)
+        .timeout(Duration.ofMillis(timeout))
+        .doOnError(error -> log.error("Error signal detected", error))
         .block();
     return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview(), movieSummary.getVote_average());
   }
