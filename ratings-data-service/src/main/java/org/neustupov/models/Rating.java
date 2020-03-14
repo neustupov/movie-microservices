cc7474f58b8dce3b6b9010fbba498ea1d875ba65
Dmitry_Neustupov <neustupov@yandex.ru>
2020-03-14 18:53:42 +0300

    Added mongo dependency for rating service

--- ratings-data-service/src/main/java/org/neustupov/models/Rating.java
+++ ratings-data-service/src/main/java/org/neustupov/models/Rating.java
@@ -3,10 +3,12 @@ package org.neustupov.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Document("rating")
 public class Rating {
 
   private String movieId;
