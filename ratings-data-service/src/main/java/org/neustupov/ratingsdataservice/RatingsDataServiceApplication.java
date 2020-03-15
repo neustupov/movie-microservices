9e94369986cc66f7da26f9a68a4631c21a0bbe90
Dmitry_Neustupov <neustupov@yandex.ru>
2020-03-15 21:52:58 +0300

    Edited mongo properties

--- ratings-data-service/src/main/java/org/neustupov/ratingsdataservice/RatingsDataServiceApplication.java
+++ ratings-data-service/src/main/java/org/neustupov/ratingsdataservice/RatingsDataServiceApplication.java
@@ -1,17 +1,36 @@
 package org.neustupov.ratingsdataservice;
 
+import org.neustupov.models.Rating;
+import org.neustupov.repository.RatingRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import reactor.core.publisher.Flux;
 
 @EnableEurekaClient
 @SpringBootApplication
 @ComponentScan("org.neustupov")
+@EnableReactiveMongoRepositories
 public class RatingsDataServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RatingsDataServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(RatingRepository ratingRepository) {
+		return args -> ratingRepository.deleteAll()
+				.thenMany(Flux.just(
+						new Rating("500", 5.0),
+						new Rating("600", 4.0)
+				)
+						.flatMap(ratingRepository::save))
+				.thenMany(ratingRepository.findAll())
+				.subscribe(System.out::println);
 	}
 
 }
